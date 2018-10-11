a57ca6870d3f67a2fd430233128ced8e12783bbc
SeriousFresh <31888733+SeriousFresh@users.noreply.github.com>
2018-10-12 01:47:13 +0200

    sredjena jos animacija

--- Pandamonium/Assets/Scripts/Animation/PlayerAnimation.cs
+++ Pandamonium/Assets/Scripts/Animation/PlayerAnimation.cs
@@ -28,45 +28,20 @@ public class PlayerAnimation : MonoBehaviour
     {
 
         Vector3 vector3D = path.velocity;
-     
         Vector2 vector2D = new Vector2(vector3D.x, vector3D.y);
-
-        //float angle = Vector3.Angle(vectorDirection, new Vector3(0 ,1,0));
-        print(vector3D);
       
         if (Mathf.Approximately(vector2D.x,Vector2.zero.x) && Mathf.Approximately(vector2D.y, Vector2.zero.y))
-        {
-            //animator.SetLayerWeight(0, 0);
             animator.SetLayerWeight(1, 0);
-            print("prvi:");
-            print(angle);
-        }
         else 
         {
-          // animator.SetLayerWeight(0, 1);
-           
-            // animator.SetFloat("Angle", angle);
             animator.SetLayerWeight(1, 1);
 
             if (vector2D.y < 0)
-            {
                 angle = 180 + 180 - Vector2.Angle(vector2D, new Vector2(1, 0));
-            }
-
             else
                 angle = Vector2.Angle(vector2D, new Vector2(1, 0));
 
-            print("drugi:");
-            print(angle);
             animator.SetFloat("Angle", angle);
-
         }
-
-        // print(angle);
-
-        // print(angle);
-
-
-
     }
 }
