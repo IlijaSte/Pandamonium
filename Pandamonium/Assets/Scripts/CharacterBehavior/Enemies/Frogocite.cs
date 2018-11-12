a54c5a1cb9c77dddbf9f3debfcc25b6506f8cf57
SeriousFresh <31888733+SeriousFresh@users.noreply.github.com>
2018-11-12 21:25:02 +0100

    sitne frogocit promene

---
+++ Pandamonium/Assets/Scripts/CharacterBehavior/Enemies/Frogocite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frogocite : Enemy
+{
+
+    public float jumpSpeed;  //allows us to be able to change speed in Unity
+    public Vector2 jumpHeight = new Vector2(0, 10);
+
+    public override void Start()
+    {
+        base.Start();
+        jumpSpeed = normalSpeed * 1.2f;
+    }
+
+    protected override void Update()
+    {
+        /*
+        if (timeToDash >= dashCooldown && playerState == PlayerState.CHASING_ENEMY && CanSee(player, maxDashRange))
+        {
+            StartCoroutine(Dash(player));
+        }
+        */
+
+
+        if ( CanSee(player, maxDashRange))
+            Jump();
+
+        base.Update();
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.AddForce(rb.velocity, ForceMode2D.Impulse);
+
+    }
+}
