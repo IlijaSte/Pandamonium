48d601c56c3523c4919dc7042d0bdc68566fcd2e
IlijaSte <ilijaste96@gmail.com>
2018-12-09 02:37:38 +0100

    ubacena mogucnost screenshota (E)

--- Pandamonium/Assets/Scripts/CharacterBehavior/TrailCollider.cs
+++ Pandamonium/Assets/Scripts/CharacterBehavior/TrailCollider.cs
@@ -16,22 +16,33 @@ public class TrailCollider : HazardousArea<PlayerWithJoystick> {
     }
 
 
-    IEnumerator ColliderLifecycle(BoxCollider2D collider, float time)
+    IEnumerator ColliderLifecycle(BoxCollider2D collider, float time, float minSize, float maxSize)
     {
-        yield return new WaitForSeconds(time);
+
+        if (collider == null)
+            yield break;
+
+        Vector2 startSize = new Vector2(minSize, minSize);
+        Vector2 endSize = new Vector2(maxSize, maxSize);
+
+        for(int i = 0; i < time; i++)
+        {
+            collider.size = Vector2.Lerp(startSize, endSize, i / time);
+            yield return new WaitForSeconds(1);
+        }
 
         if (collider)
             Destroy(collider);
 
     }
-    public void CreateCollider(Vector2 pos, float size, float time)
+    public void CreateCollider(Vector2 pos, float minSize, float maxSize, float time)
     {
         BoxCollider2D newCollider = gameObject.AddComponent<BoxCollider2D>();
 
-        newCollider.size = new Vector2(size, size) / transform.lossyScale.x;
+        //newCollider.size = new Vector2(size, size) / transform.lossyScale.x;
         newCollider.offset = pos;
         newCollider.usedByComposite = true;
 
-        StartCoroutine(ColliderLifecycle(newCollider, time));
+        StartCoroutine(ColliderLifecycle(newCollider, time, minSize, maxSize));
     }
 }
